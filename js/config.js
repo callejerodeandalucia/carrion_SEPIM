31ee59b89c3ae99bd9daeeba8a21e2593df1d570
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2016-10-20 09:07:14 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 //******** configuración **********************//
-url = "http://www.callejerodeandalucia.es/pruebas/sepim/api";
+url = "http://www.callejerodeandalucia.es/sepim/api";
 urlGB = ""; //JGL - vacío si no tiene GB
-idAplicacion = 68; //id de la aplicación
+idAplicacion = 139; //id de la aplicación
 //*********************************************//
 aplicacion = null;
 pilaCategorias = [];
